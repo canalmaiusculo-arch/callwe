70dc570a920b22111576863275f8aa0f28ecb374
canalmaiusculo-arch <canalmaiusculo@gmail.com>
2026-04-18 11:16:29 -0300

    fix: import LeadsModule in CloudtalkModule

--- apps/api/src/modules/cloudtalk/cloudtalk.module.ts
+++ apps/api/src/modules/cloudtalk/cloudtalk.module.ts
@@ -3,9 +3,10 @@ import { CloudtalkService } from './cloudtalk.service.js';
 import { CloudtalkWebhooksController } from './webhooks.controller.js';
 import { CallFlowController } from './call-flow.controller.js';
 import { RealtimeModule } from '../realtime/realtime.module.js';
+import { LeadsModule } from '../leads/leads.module.js';
 
 @Module({
-  imports: [RealtimeModule],
+  imports: [RealtimeModule, LeadsModule],
   controllers: [CloudtalkWebhooksController, CallFlowController],
   providers: [CloudtalkService],
   exports: [CloudtalkService],
